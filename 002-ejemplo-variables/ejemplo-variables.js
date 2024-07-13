69520f3a4d4cbbc503b99a8417b0923cf3957ccf
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2024-07-13 16:11:38 +0200

    first commit

---
+++ 002-ejemplo-variables/ejemplo-variables.js
@@ -0,0 +1,30 @@
+// tipo NUMBER
+var miNumEntero = 15;
+console.log(miNumEntero);
+
+var miNumDecimal = 253.81;
+console.log(miNumDecimal);
+
+let miNumEntero2 = 36;
+console.log(miNumEntero2)
+
+let miNumDecimal2 = 3.15;
+console.log(miNumDecimal2);
+
+// tipo String
+let saludo = "Hola Mundo";
+var hello = "Hello World";
+let nombre = "Paula";
+// enviar informacion a consola y concatenacion de Strings
+console.log("Los que van a programar te saludan: " + saludo + " " + nombre + " " + hello);
+
+// tipo Boolean
+let verdadero = true;
+let falso = false;
+console.log("Lo contrario de verdadero es: " + !verdadero);
+
+// tipo Undefined
+let x;
+let y;
+console.log(x);
+console.log(y);
